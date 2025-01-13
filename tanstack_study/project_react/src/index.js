e929c5a980c3b0939ee1d14b145140991fcc7a2c
Julia <102248096+JustJuulia@users.noreply.github.com>
2025-01-13 14:58:10 +0100

    cleaned file a bit

--- tanstack_study/project_react/src/index.js
+++ tanstack_study/project_react/src/index.js
@@ -2,23 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import MyOwnTable from './MyOwnTable';
-import Sci from './Sci';
-import reportWebVitals from './reportWebVitals';
-import NewComponent from './NewComponent';
-import Exercise1 from './Exercise1';
-import Exercise_2310 from './Exercise_2310';
-import AxiosExample from './AxiosExample';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NewComponent />
-    <Sci />
-    <Exercise1 />
-    <MyOwnTable/>
-    <Exercise_2310 />
-    <AxiosExample />
+    
   </React.StrictMode>
 );
 
